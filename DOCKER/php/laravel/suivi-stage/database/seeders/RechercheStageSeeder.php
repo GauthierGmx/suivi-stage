b6ef9749deb57c0411cbf13f002c7cdae5cffe1a
Gauthier GOUMEAUX <gauthier.goumeaux640@gmail.com>
2024-12-31 15:18:36 +0100

    Création des seeder

---
+++ DOCKER/php/laravel/suivi-stage/database/seeders/RechercheStageSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class RechercheStageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
