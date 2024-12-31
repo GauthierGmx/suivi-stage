52e7b01d615bdd1263c296589aaa79a8f0c5ca2a
Gauthier GOUMEAUX <gauthier.goumeaux640@gmail.com>
2024-12-31 15:58:15 +0100

    Ajout des seeders pour les tables pivot

---
+++ DOCKER/php/laravel/suivi-stage/database/seeders/EtudiantAnneeformAnneeunivSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class EtudiantAnneeformAnneeunivSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
