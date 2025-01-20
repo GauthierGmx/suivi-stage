86a60bbb055997bc152fb22010e9a8d572f890b5
Gauthier GOUMEAUX <gauthier.goumeaux640@gmail.com>
2025-01-20 18:05:17 +0100

    Ajout d'une recherche de stage fonctionnel

---
+++ DOCKER/php/laravel/suivi-stage/app/Http/Controllers/RechercheStageController.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RechercheStage;
+use Illuminate\Http\Request;
+
+class RechercheStageController extends Controller
+{
+    /**
+     * Retourne toutes les recherches de stage
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     *  Une réponse JSON avec :
+     *      - Code 201 : si l'enregistrement a été effectué avec succès
+     *      - Code 500 : en cas d'erreur
+     * @throws \Illuminate\Database\QueryException
+     * @throws \Exception
+     */
+    public function store(Request $request)
+    {
+        try
+        {
+            $donneesValidees = $request->validate([
+                'dateCreation'          => 'bail|required|date',
+                'dateModification'      => 'bail|required|date',
+                'date1erContact'        => 'bail|required|date',
+                'typeContact'           => 'bail|required|string|in:Courrier,Mail,Présentiel,Téléphone,Site de recrutement',
+                'nomContact'            => 'bail|required|string|max:50',
+                'prenomContact'         => 'bail|required|string|max:50',
+                'fonctionContact'       => 'bail|required|string|max:50',
+                'telephoneContact'      => ['nullable','string','regex:/^(\+33|0)\d{9}$/m'],
+                'adresseMailContact'    => 'nullable|string|email|max:100',
+                'observations'          => 'nullable|string',
+                'dateRelance'           => 'nullable|date',
+                'statut'                => 'bail|required|string|in:En cours,Validé,Refusé,Relancé',
+                'idUPPA'                => 'bail|required|string|exists:etudiants,idUPPA',
+                'idEntreprise'          => 'required|integer|exists:entreprises,idEntreprise',
+            ]);
+    
+            $uneRechercheStage = RechercheStage::create([
+                'dateCreation' => $donneesValidees['dateCreation'],
+                'dateModification' => $donneesValidees['dateModification'],
+                'date1erContact' => $donneesValidees['date1erContact'],
+                'typeContact' => $donneesValidees['typeContact'],
+                'nomContact' => $donneesValidees['nomContact'],
+                'prenomContact' => $donneesValidees['prenomContact'],
+                'fonctionContact' => $donneesValidees['fonctionContact'],
+                'telephoneContact' => $donneesValidees['telephoneContact'],
+                'adresseMailContact' => $donneesValidees['adresseMailContact'],
+                'observations' => $donneesValidees['observations'],
+                'dateRelance' => $donneesValidees['dateRelance'],
+                'statut' => $donneesValidees['statut'],
+                'idUPPA' => $donneesValidees['idUPPA'],
+                'idEntreprise' => $donneesValidees['idEntreprise'],
+            ]);
+    
+            return response()->json([
+                'message' => 'Recherche de stage créée avec succès',
+                'rechercheStage' => $uneRechercheStage
+            ], 201);
+        }
+        catch (\Illuminate\Database\QueryException $e)
+        {
+            return response()->json([
+                'message' => 'Erreur dans la base de données',
+                'erreurs' => $e->getMessage()
+            ], 500);
+        }
+        catch (\Exception $e)
+        {
+            return response()->json([
+                'message' => 'Une erreur s\'est produite :',
+                'erreurs' => $e->getMessage()
+            ], 500);
+        }
+    }
+    
+    /**
+     * Retourne une recherche de stage particulier
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Modifie une recherche de stage particulière
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Supprime une recherche de stage particulière
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- DOCKER/php/laravel/suivi-stage/routes/api.php
+++ DOCKER/php/laravel/suivi-stage/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RechercheStageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route pour le Controller RechercheStage
+Route::post('/recherches-stages/create', [RechercheStageController::class, 'store'])->name('recherches-stages.store');
