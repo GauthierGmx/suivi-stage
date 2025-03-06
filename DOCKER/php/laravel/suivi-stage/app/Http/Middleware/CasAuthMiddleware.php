89abcdcf44b79dcc283b540182109e675e8aca43
Gauthier GOUMEAUX <gauthier.goumeaux640@gmail.com>
2025-03-06 15:49:27 +0100

    Ecriture du middleware

--- DOCKER/php/laravel/suivi-stage/app/Http/Middleware/CasAuthMiddleware.php
+++ DOCKER/php/laravel/suivi-stage/app/Http/Middleware/CasAuthMiddleware.php
@@ -16,6 +16,55 @@ class CasAuthMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
+        // To be removed once in production
+        \phpCAS::setVerbose(true);
+        \phpCAS::setDebug(storage_path('logs/cas.log'));
+
+        // CAS client configuration
+        \phpCAS::client(
+            CAS_VERSION_2_0,
+            env('auth.cas.server.hostname'),
+            env('auth.cas.server.port'),
+            env('auth.cas.server.uri'),
+            env('auth.cas.server.basename')
+        );
+
+        // TO BE REMOVED IN PRODUCTION (This methode deactivate the SSL verification)
+        \phpCAS::setNoCasServerValidation();
+
+        // Force the user to authentify if not already done
+        if(!\phpCAS::isAuthenticated()) {
+            \phpCAS::forceAuthentication();
+        }
+
+        $userLogin = \phpCAS::getUser();
+        // Verify if the login has already been in set in session variable
+        if(!session()->has('cas_login'))
+        {
+            $user = Etudiant::where('login', $userLogin)->first();
+            $userType = "Etudiant";
+
+            // If no student found
+            if (!$user)
+            {
+                $user = Personnel::where('login', $userLogin)->first();
+                $userType = "Personnel";
+            }
+
+            // If no student and personnel found
+            if (!$user)
+            {
+                abort(403, "Vous n'êtes pas autorisé à accéder à cette page.");
+            }
+
+            // Fill the session with the user informations
+            session([
+                'cas_login' => $userLogin,
+                'id' => $userType === "Etudiant" ? $user->idUPPA : $user->idPersonnel,
+                'user' => $user,
+                'user_type' => $userType
+            ])
+        }
         return $next($request);
     }
 }
