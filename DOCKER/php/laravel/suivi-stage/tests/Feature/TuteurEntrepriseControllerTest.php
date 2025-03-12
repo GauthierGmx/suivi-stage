ef247bd54d870a7d21dfebae29df8d693d4fbb82
MaxMontouro <max.montouro@gmail.com>
2025-03-12 19:47:40 +0100

    - Ajout de tests pour la méthode store du TuteurEntrepriseController (ils passent sauf le mock)

--- DOCKER/php/laravel/suivi-stage/tests/Feature/TuteurEntrepriseControllerTest.php
+++ DOCKER/php/laravel/suivi-stage/tests/Feature/TuteurEntrepriseControllerTest.php
@@ -41,4 +41,70 @@ class TuteurEntrepriseControllerTest extends TestCase
         $response->assertStatus(200)
                  ->assertJsonCount($desTuteursEntreprises->count());
     }
+
+    /*
+    ================================
+        TEST DE LA METHODE SHOW
+    ================================
+    */
+
+    /**
+     * La méthode show va retourner une confirmation 200 et le tuteur d'entreprise demandé
+     * 
+     * @return void
+     */
+    public function test_show_renvoie_une_confirmation_et_le_tuteur_entreprise_demande(){
+        $unTuteurEntreprise = TuteurEntreprise::first();
+
+        $response = $this->get('/api/tuteur-entreprise/'.$unTuteurEntreprise->idTuteur);
+
+        $response->assertStatus(200)
+                 ->assertJson($unTuteurEntreprise->toArray());
+    }
+
+    /**
+     * La méthode show va retourner une confirmation 404 si le tuteur d'entreprise n'existe pas
+     * 
+     * @return void
+     */
+    public function test_show_renvoie_une_confirmation_404_si_le_tuteur_entreprise_n_existe_pas(){
+        $response = $this->get('/api/tuteur-entreprise/1000');
+
+        $response->assertStatus(404);
+        $response->assertJson([
+            'message' => 'Aucun tuteur d\'entreprise trouvé'
+        ]);
+    }
+
+    /**
+     * La méthode show va retourner une confirmation 500 si une erreur survient
+     * 
+     * @return void
+     */
+    public function test_show_renvoie_une_erreur_generique_en_cas_d_exception()
+    {
+        // Mock du modèle TuteurEntreprise pour déclencher une exception
+        $this->mock(\App\Http\Controllers\TuteurEntreprise::class, function ($mock) {
+            $mock->shouldReceive('show')->andThrow(new \Exception('Erreur simulée'));
+        });
+
+        $unEtudiant = Etudiant::first();
+
+        $response = $this->get('/api/etudiants/'.$unEtudiant->idUPPA);
+
+        $response->assertStatus(500)
+                 ->assertJson(['message' => 'Une erreur s\'est produite']);
+    }
+
+    /*
+    ================================
+        TEST DE LA METHODE STORE
+    ================================
+    */
+
+    /*
+    ================================
+        TEST DE LA METHODE UPDATE
+    ================================
+    */
 }
