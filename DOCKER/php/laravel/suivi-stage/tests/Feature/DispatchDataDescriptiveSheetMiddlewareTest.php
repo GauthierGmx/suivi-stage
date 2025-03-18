279a1f3a91751ebb84f622f9f88ff308bf1d0b4c
MaxMontouro <max.montouro@gmail.com>
2025-03-18 11:09:24 +0100

    - Ajout de test pour le Middleware - Ajout de la gestion des erreurs dans le Middleware en passant findOrFail à find vu qu'on gère nous même les erreurs

--- DOCKER/php/laravel/suivi-stage/tests/Feature/DispatchDataDescriptiveSheetMiddlewareTest.php
+++ DOCKER/php/laravel/suivi-stage/tests/Feature/DispatchDataDescriptiveSheetMiddlewareTest.php
@@ -4,6 +4,11 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\FicheDescriptive;
+use App\Models\Entreprise;
+use App\Models\TuteurEntreprise;
+use App\Models\Etudiant;
+use App\Http\Middleware\DispatchDataDescriptiveSheet;
 use Tests\TestCase;
 
 class DispatchDataDescriptiveSheetMiddlewareTest extends TestCase
@@ -35,8 +40,261 @@ class DispatchDataDescriptiveSheetMiddlewareTest extends TestCase
 
     public function test_handleSheetGet_renvoie_une_confirmation_et_les_infos_de_la_fiche_descriptive(){
 
-        $response = $this->get('/api/fiche-descriptive/1');
-        $response->assertStatus(200);
-                
+        $ficheDescriptive = FicheDescriptive::first();
+        $idEntreprise = $ficheDescriptive->idEntreprise;
+        $entreprise = Entreprise::find($idEntreprise);
+        $idTuteurEntreprise = $ficheDescriptive->idTuteurEntreprise;
+        $tuteur = TuteurEntreprise::find($idTuteurEntreprise);
+        $idEtudiant = $ficheDescriptive->idUPPA;
+        $etudiant = Etudiant::find($idEtudiant);
+        $response = $this->get('/api/fiche-descriptive/'.$ficheDescriptive->idFicheDescriptive);
+        $response->assertStatus(200)
+                 ->assertJson([
+                    //Informations Fiche Descriptive
+                     'idFicheDescriptive' => [
+                        'value' => $ficheDescriptive->idFicheDescriptive,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'dateCreationFicheDescriptive' => [
+                        'value' => $ficheDescriptive->dateCreation,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'dateDerniereModificationFicheDescriptive' => [
+                        'value' => $ficheDescriptive->dateDerniereModification,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'contenuStageFicheDescriptive' => [
+                        'value' => $ficheDescriptive->contenuStage,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'thematiqueFicheDescriptive' => [
+                        'value' => $ficheDescriptive->thematique,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'sujetFicheDescriptive' => [
+                        'value' => $ficheDescriptive->sujet,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'fonctionsFicheDescriptive' => [
+                        'value' => $ficheDescriptive->fonctions,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'tachesFicheDescriptive' => [
+                        'value' => $ficheDescriptive->taches,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'competencesFicheDescriptive' => [
+                        'value' => $ficheDescriptive->competences,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'detailsFicheDescriptive' => [
+                        'value' => $ficheDescriptive->details,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'debutStageFicheDescriptive' => [
+                        'value' => $ficheDescriptive->debutStage,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'finStageFicheDescriptive' => [
+                        'value' => $ficheDescriptive->finStage,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'nbJourSemaineFicheDescriptive' => [
+                        'value' => $ficheDescriptive->nbJourSemaine,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'nbHeureSemaineFicheDescriptive' => [
+                        'value' => $ficheDescriptive->nbHeureSemaine,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'clauseConfidentialiteFicheDescriptive' => [
+                        'value' => $ficheDescriptive->clauseConfidentialite,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'serviceEntrepriseFicheDescriptive' => [
+                        'value' => $ficheDescriptive->serviceEntreprise,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'adresseMailStageFicheDescriptive' => [
+                        'value' => $ficheDescriptive->adresseMailStage,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'telephoneStageFicheDescriptive' => [
+                        'value' => $ficheDescriptive->telephoneStage,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'adresseStageFicheDescriptive' => [
+                        'value' => $ficheDescriptive->adresseStage,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'codePostalStageFicheDescriptive' => [
+                        'value' => $ficheDescriptive->codePostalStage,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'villeStageFicheDescriptive' => [
+                        'value' => $ficheDescriptive->villeStage,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'paysStageFicheDescriptive' => [
+                        'value' => $ficheDescriptive->paysStage,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'statut' => [
+                        'value' => $ficheDescriptive->statut,
+                        'type' => 'ficheDescriptive',
+                    ],
+                     'numeroConventionFicheDescriptive' => [
+                        'value' => $ficheDescriptive->numeroConvention,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'interruptionStageFicheDescriptive' => [
+                        'value' => $ficheDescriptive->interruptionStage,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'dateDebutInterruptionFicheDescriptive' => [
+                        'value' => $ficheDescriptive->dateDebutInterruption,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'dateFinInterruptionFicheDescriptive' => [
+                        'value' => $ficheDescriptive->dateFinInterruption,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'personnelTechniqueDisponibleFicheDescriptive' => [
+                        'value' => $ficheDescriptive->personnelTechniqueDisponible,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'materielPreteFicheDescriptive' => [
+                        'value' => $ficheDescriptive->materielPrete,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'idEntreprise' => [
+                        'value' => $ficheDescriptive->idEntreprise,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     'idTuteurEntreprise' => [
+                        'value' => $ficheDescriptive->idTuteurEntreprise,
+                        'type' => 'ficheDescriptive',
+                     ],
+                     //Informations étudiant 
+                     'idUPPA' => [
+                        'value' => $etudiant->idUPPA,
+                        'type' => 'etudiant',
+                    ],
+                     'nomEtudiant' => [
+                         'value' => $etudiant->nom,
+                         'type' => 'etudiant',
+                     ],
+                     'prenomEtudiant' => [
+                         'value' => $etudiant->prenom,
+                         'type' => 'etudiant',
+                     ],
+                     'telephoneEtudiant' => [
+                         'value' => $etudiant->telephone,
+                         'type' => 'etudiant',
+                     ],
+        
+                     // Informations du tuteur
+                     'nomTuteurEntreprise' => [
+                         'value' => $tuteur->nom,
+                         'type' => 'tuteurEntreprise',
+                     ],
+                     'prenomTuteurEntreprise' => [
+                         'value' => $tuteur->prenom,
+                         'type' => 'tuteurEntreprise',
+                     ],
+                     'telephoneTuteurEntreprise' => [
+                         'value' => $tuteur->telephone,
+                         'type' => 'tuteurEntreprise',
+                     ],
+                     'adresseMailTuteurEntreprise' => [
+                         'value' => $tuteur->adresseMail,
+                         'type' => 'tuteurEntreprise',
+                     ],
+                     'fonctionTuteurEntreprise' => [
+                         'value' => $tuteur->fonction,
+                         'type' => 'tuteurEntreprise',
+                     ],
+         
+                     // Informations de l'entreprise
+                     'numSIRETEntreprise' => [
+                        'value' => $entreprise->numSIRET,
+                        'type' => 'entreprise',
+                    ],
+                     'raisonSocialeEntreprise' => [
+                         'value' => $entreprise->raisonSociale,
+                         'type' => 'entreprise',
+                     ],
+                     'adresseEntreprise' => [
+                         'value' => $entreprise->adresse,
+                         'type' => 'entreprise',
+                     ],
+                     'typeEtablissementEntreprise' => [
+                         'value' => $entreprise->typeEtablissement,
+                         'type' => 'entreprise',
+                     ],
+                     'telephoneEntreprise' => [
+                        'value' => $entreprise->telephone,
+                        'type' => 'entreprise',
+                    ],
+                     'codePostalEntreprise' => [
+                         'value' => $entreprise->codePostal,
+                         'type' => 'entreprise',
+                     ],
+                     'villeEntreprise' => [
+                         'value' => $entreprise->ville,
+                         'type' => 'entreprise',
+                     ],
+                     'paysEntreprise' => [
+                         'value' => $entreprise->pays,
+                         'type' => 'entreprise',
+                     ],
+                     'codeAPE_NAFEntreprise' => [
+                         'value' => $entreprise->codeAPE_NAF,
+                         'type' => 'entreprise',
+                     ],
+                     'statutJuridiqueEntreprise' => [
+                         'value' => $entreprise->statutJuridique,
+                         'type' => 'entreprise',
+                     ],
+                     'effectifEntreprise' => [
+                         'value' => $entreprise->effectif,
+                         'type' => 'entreprise',
+                     ],
+                     'nomRepresentantEntreprise' => [
+                         'value' => $entreprise->nomRepresentant,
+                         'type' => 'entreprise',
+                     ],
+                     'prenomRepresentantEntreprise' => [
+                         'value' => $entreprise->prenomRepresentant,
+                         'type' => 'entreprise',
+                     ],
+                     'telephoneRepresentantEntreprise' => [
+                         'value' => $entreprise->telephoneRepresentant,
+                         'type' => 'entreprise',
+                     ],
+                     'adresseMailRepresentantEntreprise' => [
+                         'value' => $entreprise->adresseMailRepresentant,
+                         'type' => 'entreprise',
+                     ],
+                     'fonctionRepresentantEntreprise' => [
+                         'value' => $entreprise->fonctionRepresentant,
+                         'type' => 'entreprise',
+                     ],
+         
+                 ]);
+    }
+
+    /**
+     * La méthode handleSheetGet va retourner une erreur 404 si la fiche descriptive n'existe pas
+     * 
+     * @return void
+     */
+    public function test_handleSheetGet_renvoie_une_erreur_404_si_la_fiche_descriptive_n_existe_pas(){
+        $ficheDescriptive = 100000;
+        $response = $this->get('/api/fiche-descriptive/'.$ficheDescriptive);
+        $response->assertStatus(404)
+                 ->assertJson([
+                    'message' => 'Fiche descriptive non trouvée'
+                ]);
     }
 }
